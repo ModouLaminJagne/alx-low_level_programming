d04001f3d48b3123c567bf24355b382d8ddf536f
ModouLaminJagne <jagnemlgh@gmail.com>
2023-03-22 08:09:28 -0700

    By considering the terms in the Fibonacci sequence whose values do not exceed 4,000,000, write a program that finds and prints the sum of the even-valued terms, followed by a new line.

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,14 +3,14 @@
  * main -  prints the first 50 Fibonacci numbers, starting with 1 and 2
  * Return: always 0
  */
-int main (void)
+int main(void)
 {
 	int i = 0;
 	long j = 1, k = 2, sum = k;
 
 	while (k * j < 4000000)
 	{
-		k += j;
+		sum += j;
 		j = k - j;
 		++i;
 	}
