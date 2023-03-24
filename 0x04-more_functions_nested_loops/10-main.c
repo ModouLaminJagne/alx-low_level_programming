9e0d755388c29ccfb7929df189dbb034ab308653
ModouLaminJagne <jagnemlgh@gmail.com>
2023-03-23 21:29:26 -0700

    Write a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -7,12 +7,7 @@
  * Return: Always 0.
  */
 int main(void)
-{
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    
+{	
     print_triangle(2);
     print_triangle(10);
     print_triangle(1);
