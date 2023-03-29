a61533dd6482340fb3d9b7274aa195b0e9bada0b
ModouLaminJagne <jagnemlgh@gmail.com>
2023-03-28 18:04:25 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - Takes a pointer and updates
+ * the value it points to ro 98
+ * @n: An integer input poniter
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character in the stdout
+ * @c: The character to be printed
+ * Return: On success 1, on error -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
