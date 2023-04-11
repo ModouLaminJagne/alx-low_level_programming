39e03f8e7a43097f66e8df5b326d14ba9071079f
ModouLaminJagne <jagnemlgh@gmail.com>
2023-04-11 07:37:00 -0700

    Write a program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,11 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i, d, n, len = 0, f = 0, digit = 0;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
 
 	while (s[len] != '\0')
 	{
@@ -37,7 +34,7 @@ int _atoi(char *s)
 			n = n * 10 + digit;
 			f = 1;
 
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < 0 || s[i + 1] > 9)
 			{
 				break;
 			}
