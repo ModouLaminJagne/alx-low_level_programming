959d2860ead2f743c98042b623e22d4a5492f6f0
ModouLaminJagne <jagnemlgh@gmail.com>
2023-04-17 14:50:14 -0700

    add readme and header files with a function that creates a variable of type struct.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,6 +5,7 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
+ *
  * Description: Longer description
  */
 struct dog
@@ -15,6 +16,7 @@ struct dog
 };
 /**
  * dog_t - typedef for struct dog
+ *
  */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
