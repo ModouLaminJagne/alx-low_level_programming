a376556304647533b3ecb44d912ba8d9c7485437
ModouLaminJagne <jagnemlgh@gmai.com>
2023-05-02 11:47:08 -0700

    Write a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -53,26 +53,26 @@ size_t looped_listint_len(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, index;
+	size_t nodes, index = 0;
 
 	nodes = looped_listint_len(head);
 	if (nodes == 0)
 	{
-		while (; head != NULL)
+		while (head != NULL)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
-		node++;
+		nodes++;
 	}
 	else
 	{
-		while (index = 0; index < nodes)
+		while (index < nodes)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
-		index;
+		index++;
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (nodes);
